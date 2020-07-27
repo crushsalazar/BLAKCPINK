499fb1c61e1174d6f8f14c3d4e42eb3fd3856aca
Your Name <you@example.com>
2020-07-26 21:57:21 -0500

    primer app

---
+++ app/src/main/java/com/example/blackpinkcrush/JUEGOS.java
@@ -0,0 +1,14 @@
+package com.example.blackpinkcrush;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class JUEGOS extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_j_u_e_g_o_s);
+    }
+}
